22a9b5a3ae6d8ed1cb28e6173a9aa5d5d08171cd
Jason Spann <jspann3@murraystate.edu>
2017-01-20 15:00:51 -0600

    Add simple Tag design

---
+++ TCPServer/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Tag
+    {
+        public int id { get; }
+        public int color { get; }
+
+        public Tag(int ID, int Color)
+        {
+            id = ID;
+            Color = color;
+        }
+    }
+}

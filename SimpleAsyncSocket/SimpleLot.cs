e5c35c3fbf3d8323641d66179ddacbe43fda6a08
Jason Spann <jspann3@murraystate.edu>
2017-01-30 20:54:26 -0600

    Add SimpleLot to client

---
+++ SimpleAsyncSocket/SimpleLot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    class SimpleLot
+    {
+        public string id { get; }
+        public string color { get; }
+        public int spotsRemaining { get; set; }
+
+        public SimpleLot(string ID, string Color, int SpotsRemaining)
+        {
+            id = ID;
+            color = Color;
+            spotsRemaining = SpotsRemaining;
+        }
+    }
+}
